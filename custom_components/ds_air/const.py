4fed96d9d24f25476b86bbe2693648d1037798ca
maao514 <55963962+maao514@users.noreply.github.com>
2022-07-13 17:34:04 +0800

    Update const.py

--- custom_components/ds_air/const.py
+++ custom_components/ds_air/const.py
@@ -4,7 +4,7 @@ from homeassistant.const import TEMP_CELSIUS, PERCENTAGE, CONCENTRATION_MICROGRA
 
 from .ds_air_service.ctrl_enum import EnumSensor
 
-DOMAIN = "ds_air"
+DOMAIN = "ds_airrr"
 CONF_GW = "gw"
 DEFAULT_HOST = "192.168.1."
 DEFAULT_PORT = 8008
